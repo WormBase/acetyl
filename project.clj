0658f69d6d42dcec385a0c8e740bb292b92e81fd
Thomas Down <thomas.a.down@gmail.com>
2014-08-20 14:17:08 +0100

    Preliminary clojure API.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject acetyl "0.0.1-SNAPSHOT"
+(defproject acetyl "0.0.2-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :description "JVM client for AceDB"
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
+  :javac-options ["-target" "1.6" "-source" "1.6"]
   :license "LGPL"
   :url "http://www.biodalliance.org/people/thomas")
