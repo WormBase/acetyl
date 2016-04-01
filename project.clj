5bddb353677bb8737b5cb62aa81bbbb272776392
Matt Russell <matt@mgrbyte.co.uk>
2016-04-01 17:07:15 +0100

    Update project description.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject wormbase/acetyl "0.0.9-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-time "0.11.0"]]
-  :description "AceDB utility routines"
+  :description "AceDB utility routines."
   :javac-options ["-target" "1.8" "-source" "1.8"]
   :license "LGPL"
   :url "http://github.com/Wormbase/acetyl")
